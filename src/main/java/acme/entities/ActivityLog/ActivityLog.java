8b71d96e7c65aa3d29d0565f1d79f126ee0e3faa
Glinbor10 <guillelinares11@gmail.com>
2025-03-08 17:33:11 +0100

    feat: Create Activity Log and relation

--- src/main/java/acme/entities/ActivityLog/ActivityLog.java
+++ src/main/java/acme/entities/ActivityLog/ActivityLog.java
@@ -1,25 +1,28 @@
 
-package acme.entities.Assignment;
+package acme.entities.ActivityLog;
 
 import java.util.Date;
 
 import javax.persistence.Entity;
+import javax.persistence.ManyToOne;
 import javax.persistence.Temporal;
 import javax.persistence.TemporalType;
+import javax.validation.constraints.NotBlank;
 
 import acme.client.components.basis.AbstractEntity;
 import acme.client.components.mappings.Automapped;
 import acme.client.components.validation.Mandatory;
-import acme.client.components.validation.Optional;
 import acme.client.components.validation.ValidMoment;
+import acme.client.components.validation.ValidNumber;
 import acme.client.components.validation.ValidString;
+import acme.entities.Assignment.Assignment;
 import lombok.Getter;
 import lombok.Setter;
 
 @Entity
 @Getter
 @Setter
-public class Assignment extends AbstractEntity {
+public class ActivityLog extends AbstractEntity {
 
 	// Serialisation version -------------------------------------------
 
@@ -28,34 +31,31 @@ public class Assignment extends AbstractEntity {
 	// Attributes --------------------------------------------------------
 
 	@Mandatory
-	@Automapped
-	private Role				role;
-
-	@Mandatory
 	@ValidMoment(past = true)
 	@Temporal(TemporalType.TIMESTAMP)
-	private Date				lastUpdate;
+	private Date				registeredAt;
 
 	@Mandatory
+	@NotBlank
+	@ValidString(max = 50)
 	@Automapped
-	private AssignmentStatus	status;
+	private String				incidentType;
 
-	@Optional
+	@Mandatory
+	@NotBlank
 	@ValidString(max = 255)
 	@Automapped
-	private String				remarks;
+	private String				description;
 
-	/*
-	 * FALTA LA RELACIÓN CON LEG
-	 * SERÍA ASÍ LA RELACIÓN: Assignment 4..N -> 1 Leg
-	 * 
-	 * @ManyToOne
-	 * private Leg legs;
-	 * 
-	 * @Optional
-	 * 
-	 * @ManyToOne
-	 * private FlightCrewMember flightCrewMember
-	 */
+	@Mandatory
+	@ValidNumber(min = 0, max = 10, integer = 2, fraction = 0)
+	@Automapped
+	private Integer				severityLevel;
+
+	// Relationships -----------------------------------------------------
+
+	@Mandatory
+	@ManyToOne
+	private Assignment			assignment;
 
 }
