da2bb3290e30c1a1e7f5725615cb9e3e80d6647b
SANTIA BREGU <sanbre@alum.us.es>
2025-03-08 11:20:07 +0100

    feat: Create Manager Entity

---
+++ src/main/java/acme/entities/AirlineManager.java
@@ -0,0 +1,44 @@
+
+package acme.entities;
+
+import java.util.Date;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+
+import acme.client.components.basis.AbstractRole;
+import acme.client.components.mappings.Automapped;
+import acme.client.components.validation.Mandatory;
+import acme.client.components.validation.Optional;
+import acme.client.components.validation.ValidMoment;
+import acme.client.components.validation.ValidString;
+import acme.client.components.validation.ValidUrl;
+import lombok.Getter;
+import lombok.Setter;
+
+@Entity
+@Getter
+@Setter
+public class AirlineManager extends AbstractRole {
+
+	private static final long	serialVersionUID	= 1L;
+
+	@Mandatory
+	@Automapped
+	@ValidString(pattern = "^[A-Z]{2-3}\\d{6}$")
+	@Column(unique = true)
+	private String				identifier;
+
+	@Mandatory
+	@Automapped
+	private Integer				yearsOfExperience;
+
+	@Mandatory
+	@ValidMoment(past = true)
+	private Date				dateOfBirth;
+
+	@Optional
+	@ValidUrl
+	@Automapped
+	private String				pictureUrl; // Optional link to picture
+}
