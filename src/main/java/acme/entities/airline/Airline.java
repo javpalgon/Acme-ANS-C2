1bee0800f5d2f73bdffc15549a595cab22d328c1
SANTIA BREGU <sanbre@alum.us.es>
2025-03-09 11:53:20 +0100

    feat: Create airline entity

---
+++ src/main/java/acme/entities/airline/Airline.java
@@ -0,0 +1,65 @@
+
+package acme.entities.airline;
+
+import java.util.Date;
+
+import javax.persistence.Column;
+import javax.persistence.Temporal;
+import javax.persistence.TemporalType;
+import javax.validation.Valid;
+import javax.validation.constraints.NotBlank;
+
+import acme.client.components.basis.AbstractRealm;
+import acme.client.components.mappings.Automapped;
+import acme.client.components.validation.Mandatory;
+import acme.client.components.validation.Optional;
+import acme.client.components.validation.ValidMoment;
+import acme.client.components.validation.ValidString;
+import acme.client.components.validation.ValidUrl;
+import acme.constraints.ValidIATACode;
+
+public class Airline extends AbstractRealm {
+
+	// Serialisation version -------------------------------------------
+
+	private static final long	serialVersionUID	= 1L;
+
+	// Attributes --------------------------------------------------------
+
+	@Mandatory
+	@NotBlank
+	@ValidString(max = 50)
+	@Automapped
+	private String				name;
+
+	@Mandatory
+	@ValidIATACode
+	@Column(unique = true)
+	private String				IATACode;
+
+	@Mandatory
+	@ValidUrl
+	@Automapped
+	private String				website;
+
+	@Mandatory
+	@Valid
+	@Automapped
+	private Type				type;
+
+	@Mandatory
+	@ValidMoment(past = true)
+	@Temporal(TemporalType.TIMESTAMP)
+	private Date				foundationMoment;
+
+	@Optional
+	@ValidString(max = 50)
+	@Automapped
+	private String				email;
+
+	@Optional
+	@ValidString(pattern = "^\\+?\\d{6,15}$")
+	@Automapped
+	private String				phoneNumber;
+
+}
