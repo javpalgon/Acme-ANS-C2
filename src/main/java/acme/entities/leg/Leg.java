db7df17ff99b1508a482b9c9ea2b6740b5fe8e5f
SANTIA BREGU <sanbre@alum.us.es>
2025-03-09 10:12:54 +0100

    feat: Create leg entity

---
+++ src/main/java/acme/entities/leg/Leg.java
@@ -0,0 +1,74 @@
+
+package acme.entities.leg;
+
+import javax.persistence.Column;
+import javax.validation.Valid;
+
+import acme.client.components.basis.AbstractEntity;
+import acme.client.components.datatypes.Moment;
+import acme.client.components.mappings.Automapped;
+import acme.client.components.validation.Mandatory;
+import acme.client.components.validation.ValidMoment;
+import acme.client.components.validation.ValidNumber;
+import acme.client.components.validation.ValidString;
+
+public class Leg extends AbstractEntity {
+
+	// Serialisation version -------------------------------------------
+	private static final long	serialVersionUID	= 1L;
+
+	@ValidString(pattern = "^[A-Z]{2,3}\\d{4}$")
+	@Column(unique = true)
+	@Mandatory
+	private String				flightNumber;
+
+	@ValidMoment
+	@Mandatory
+	@Automapped
+	private Moment				departure;
+
+	@ValidMoment
+	@Mandatory
+	@Automapped
+	private Moment				arrival;
+
+	@ValidNumber(min = 0.1, max = 20.)
+	@Mandatory
+	@Automapped
+	private Double				duration;
+
+	@Mandatory
+	@Automapped
+	@Valid
+	private LegStatus			status;
+
+	// Relations --------------------------------------------------------
+
+	/*
+	 * (Leg N -> 1 (departure) Airport )
+	 * 
+	 * @ManyToOne
+	 * 
+	 * @
+	 * private Airport departureAP;
+	 */
+
+	/*
+	 * (Leg N -> 1 (arrival) Airport )
+	 * 
+	 * @ManyToOne
+	 * 
+	 * @
+	 * private Airport arrivalAP;
+	 */
+
+	/*
+	 * (Leg 1 -> 1 Aircraft )
+	 * 
+	 * @OneToOne
+	 * 
+	 * @
+	 * private Aircraft aircraft;
+	 */
+
+}
