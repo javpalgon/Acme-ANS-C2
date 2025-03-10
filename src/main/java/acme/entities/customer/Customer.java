78228ebbe00a4cadae5f3a5924f5db63e6ec6899
raqgarhor <raqgarhor@alum.us.es>
2025-03-10 15:13:45 +0100

    feat: update create customer entity

--- src/main/java/acme/entities/customer/Customer.java
+++ src/main/java/acme/entities/customer/Customer.java
@@ -3,13 +3,12 @@ package acme.entities.customer;
 
 import javax.persistence.Column;
 import javax.persistence.Entity;
-import javax.validation.constraints.Max;
-import javax.validation.constraints.Min;
 import javax.validation.constraints.NotBlank;
 
 import acme.client.components.mappings.Automapped;
 import acme.client.components.validation.Mandatory;
 import acme.client.components.validation.Optional;
+import acme.client.components.validation.ValidNumber;
 import acme.client.components.validation.ValidString;
 import lombok.Getter;
 import lombok.Setter;
@@ -51,8 +50,7 @@ public class Customer {
 	private String				country;
 
 	@Optional
-	@Min(0)
-	@Max(500000)
+	@ValidNumber(min = 0, max = 500000)
 	@Automapped
 	private Integer				earnedPoints;
 
