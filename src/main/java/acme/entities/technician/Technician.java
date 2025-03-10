58fbeaea429b89410f9432f558d459ff0579fd39
darrodsas <darrodsas@alum.us.es>
2025-03-10 11:46:48 +0100

    fix: fixed classpath conflict

--- src/main/java/acme/entities/technician/Technician.java
+++ src/main/java/acme/entities/technician/Technician.java
@@ -1,49 +1,55 @@
 
 package acme.entities.technician;
 
+import javax.persistence.Column;
 import javax.persistence.Entity;
-import javax.validation.constraints.Size;
+import javax.validation.Valid;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.PositiveOrZero;
 
 import acme.client.components.basis.AbstractEntity;
 import acme.client.components.mappings.Automapped;
 import acme.client.components.validation.Mandatory;
-import acme.client.components.validation.Optional;
+import acme.client.components.validation.ValidString;
 import lombok.Getter;
 import lombok.Setter;
 
 @Entity
 @Getter
 @Setter
+
 public class Technician extends AbstractEntity {
 
 	private static final long	serialVersionUID	= 1L;
 
 	@Mandatory
 	@Automapped
-	//Crear Validación
+	@NotBlank
+	@ValidString(pattern = "^[A-Z]{2-3}\\d{6}$")
+	@Column(unique = true)
 	private String				licenseNumber;
 
 	@Mandatory
 	@Automapped
-	//Crear Validación
+	@NotBlank
+	@ValidString(pattern = "^+?\\d{6,15}$")
+	@Column(unique = true)
 	private String				phoneNumber;
 
 	@Mandatory
-	@Size(min = 0, max = 50)
+	@NotBlank
+	@ValidString(max = 50)
 	@Automapped
 	private String				specialisation;
 
 	@Mandatory
+	@Valid
 	@Automapped
 	private Boolean				passedHealthTet;
 
 	@Mandatory
+	@PositiveOrZero
 	@Automapped
 	private Integer				yearsOfExperience;
-
-	@Optional
-	@Size(min = 0, max = 255)
-	@Automapped
-	private String				certifications;
 
 }
