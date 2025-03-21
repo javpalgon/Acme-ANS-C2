bebeba05be8e1e2828bad103374c23bec65626f8
Glinbor10 <guillelinares11@gmail.com>
2025-03-21 10:38:05 +0100

    fix: Some grupal errors

--- src/main/java/acme/entities/passenger/Passenger.java
+++ src/main/java/acme/entities/passenger/Passenger.java
@@ -1,5 +1,5 @@
 
-package acme.realms;
+package acme.entities.passenger;
 
 import java.util.Date;
 
@@ -9,7 +9,7 @@ import javax.persistence.Temporal;
 import javax.persistence.TemporalType;
 import javax.validation.constraints.NotBlank;
 
-import acme.client.components.basis.AbstractRole;
+import acme.client.components.basis.AbstractEntity;
 import acme.client.components.mappings.Automapped;
 import acme.client.components.validation.Mandatory;
 import acme.client.components.validation.Optional;
@@ -22,7 +22,7 @@ import lombok.Setter;
 @Entity
 @Getter
 @Setter
-public class Passenger extends AbstractRole {
+public class Passenger extends AbstractEntity {
 
 	private static final long	serialVersionUID	= 1L;
 
